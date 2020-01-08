5c643de492ee70b9e0460b17a94e7bf429e74de5
Mr2803 <rispoli.mar2803@gmail.com>
2020-01-08 14:24:20 +0100

    finish exercise... ?

--- getAllConf.php
+++ getAllConf.php
@@ -3,33 +3,72 @@
 class Configurazione {
 
   // variabili
+  public $id;
+  public $title;
+  public $description;
 
-  function __construct(/* parametri*/) {
+  function __construct($id, $title, $description) {
 
     // valorizzazione variabili tramite parametri
+    $this -> id = $id;
+    $this -> title = $title;
+    $this -> description = $description;
   }
 
   //funzioni utili
 
   public function __toString() {
 
-    return /* rappresentazione testuale dell'oggetto */;
+    /* rappresentazione testuale dell'oggetto */
+    return "[" . $this->id . "] "
+      . $this->title . " - "
+      . $this->description;
   }
 }
 
 // connessione al DB
+header('Content-Type: application/json');
+
+$server = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "hoteldb";
+
+$conn = new mysqli($server, $username, $password, $dbname);
+if ($conn->connect_errno) {
+
+  echo json_encode(-1);
+  return;
+}
+
 
 // download di tutte le configurazioni
 
+$sql = "
+
+      SELECT *
+      FROM configurazioni
+
+  ";
+$res = $conn->query($sql);
+if ($res->num_rows < 1) {
+
+  echo json_encode(-2);
+  return;
+}
+
 $confs = [];
-while($conf = $res -> fech_assoc()) {
+while ($conf = $res->fetch_assoc()) {
 
-  $confs[] = new Configurazione($conf['id'],
-                                $conf['title'],
-                                $conf['description']);
+  $myConf = new Configurazione(
+    $conf['id'],
+    $conf['title'],
+    $conf['description']
+  );
+  $confs[] = $myConf;
 }
 
-foreach ($conf as $confs) {
-
-  echo $conf;
+foreach ($confs as $key => $value) {
+  echo $value . "<br />";
 }
+
